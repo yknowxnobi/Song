122a06d1c391668049d8bfd4e89e7a490f8b0c84
Soumyadeep Das <133207548+Soumyadeep765@users.noreply.github.com>
2024-11-08 08:27:54 +0530

    Update spsearch.js

--- spsearch.js
+++ spsearch.js
@@ -12,8 +12,13 @@ const getAccessToken = async () => {
     },
     data: 'grant_type=client_credentials'
   };
-  const response = await axios.post(tokenUrl, authOptions.data, { headers: authOptions.headers });
-  return response.data.access_token;
+
+  try {
+    const response = await axios.post(tokenUrl, authOptions.data, { headers: authOptions.headers });
+    return response.data.access_token;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const searchSongs = async (accessToken, query, limit = 30, offset = 0) => {
@@ -29,63 +34,82 @@ const searchSongs = async (accessToken, query, limit = 30, offset = 0) => {
       offset
     }
   };
-  const response = await axios.get(searchUrl, options);
-  return response.data;
+
+  try {
+    const response = await axios.get(searchUrl, options);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getAlbumDetails = async (accessToken, albumUrl) => {
   const albumId = albumUrl.split('/').pop().split('?')[0];
   const albumDetailsUrl = `https://api.spotify.com/v1/albums/${albumId}`;
-  const response = await axios.get(albumDetailsUrl, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
-  });
-  const albumInfo = response.data;
-  const tracks = albumInfo.tracks.items.map((track, index) => ({
-    trackNumber: index + 1,
-    trackName: track.name,
-    artist: track.artists.map(artist => artist.name).join(', '),
-    durationMs: track.duration_ms,
-    previewUrl: track.preview_url,
-    spotifyUrl: track.external_urls.spotify
-  }));
-  return {
-    albumName: albumInfo.name,
-    artist: albumInfo.artists.map(artist => artist.name).join(', '),
-    releaseDate: albumInfo.release_date,
-    imageUrl: albumInfo.images[0].url,
-    totalTracks: albumInfo.total_tracks,
-    tracks
-  };
+
+  try {
+    const response = await axios.get(albumDetailsUrl, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+    const albumInfo = response.data;
+    const tracks = albumInfo.tracks.items.map((track, index) => ({
+      trackNumber: index + 1,
+      trackName: track.name,
+      artist: track.artists.map(artist => artist.name).join(', '),
+      durationMs: track.duration_ms,
+      previewUrl: track.preview_url,
+      spotifyUrl: track.external_urls.spotify
+    }));
+
+    return {
+      albumName: albumInfo.name,
+      artist: albumInfo.artists.map(artist => artist.name).join(', '),
+      releaseDate: albumInfo.release_date,
+      imageUrl: albumInfo.images[0].url,
+      totalTracks: albumInfo.total_tracks,
+      tracks
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getPlaylistDetails = async (accessToken, playlistUrl) => {
   const playlistId = playlistUrl.split('/').pop().split('?')[0];
   const playlistDetailsUrl = `https://api.spotify.com/v1/playlists/${playlistId}`;
-  const response = await axios.get(playlistDetailsUrl, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
-  });
-  const playlistInfo = response.data;
-  const tracks = playlistInfo.tracks.items.map((item, index) => ({
-    trackNumber: index + 1,
-    trackName: item.track.name,
-    artist: item.track.artists.map(artist => artist.name).join(', '),
-    album: item.track.album.name,
-    durationMs: item.track.duration_ms,
-    previewUrl: item.track.preview_url,
-    spotifyUrl: item.track.external_urls.spotify
-  }));
-  return {
-    playlistName: playlistInfo.name,
-    description: playlistInfo.description,
-    owner: playlistInfo.owner.display_name,
-    imageUrl: playlistInfo.images[0].url,
-    totalTracks: playlistInfo.tracks.total,
-    tracks
-  };
+
+  try {
+    const response = await axios.get(playlistDetailsUrl, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+
+    const playlistInfo = response.data;
+    const tracks = playlistInfo.tracks.items.map((item, index) => ({
+      trackNumber: index + 1,
+      trackName: item.track.name,
+      artist: item.track.artists.map(artist => artist.name).join(', '),
+      album: item.track.album.name,
+      durationMs: item.track.duration_ms,
+      previewUrl: item.track.preview_url,
+      spotifyUrl: item.track.external_urls.spotify
+    }));
+
+    return {
+      playlistName: playlistInfo.name,
+      description: playlistInfo.description,
+      owner: playlistInfo.owner.display_name,
+      imageUrl: playlistInfo.images[0].url,
+      totalTracks: playlistInfo.tracks.total,
+      tracks
+    };
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = async (req, res) => {
@@ -98,15 +122,26 @@ module.exports = async (req, res) => {
   const bot = req.query.bot;
 
   try {
-    const accessToken = await getAccessToken();
     if (spotifyUrl && isPlaylist) {
+      const accessToken = await getAccessToken();
       const playlistDetails = await getPlaylistDetails(accessToken, spotifyUrl);
-      return res.status(200).json({ data: playlistDetails, developerCredit: 'https://t.me/Teleservices_Api' });
+
+      return res.status(200).json({
+        data: playlistDetails,
+        developerCredit: 'https://t.me/Teleservices_Api'
+      });
     } else if (spotifyUrl && isAlbum) {
+      const accessToken = await getAccessToken();
       const albumDetails = await getAlbumDetails(accessToken, spotifyUrl);
-      return res.status(200).json({ tracks: albumDetails.tracks, developerCredit: 'https://t.me/Teleservices_Api' });
+
+      return res.status(200).json({
+        tracks: albumDetails.tracks,
+        developerCredit: 'https://t.me/Teleservices_Api'
+      });
     } else if (spotifyUrl) {
+      const accessToken = await getAccessToken();
       const trackDetails = await searchSongs(accessToken, spotifyUrl, 1, 0);
+
       const trackInfo = trackDetails.tracks.items[0];
       if (trackInfo) {
         return res.status(200).json({
@@ -125,17 +160,15 @@ module.exports = async (req, res) => {
         return res.status(404).json({ error: 'Track not found' });
       }
     } else if (query) {
-      let tracks = [];
-      const firstBatch = await searchSongs(accessToken, query, Math.min(limit, 100), offset);
-      tracks = tracks.concat(firstBatch.tracks.items);
-      if (limit > 100 && firstBatch.tracks.items.length === 100) {
-        const secondBatch = await searchSongs(accessToken, query, Math.min(limit - 100, 100), offset + 100);
-        tracks = tracks.concat(secondBatch.tracks.items);
-      }
+      const accessToken = await getAccessToken();
+      const searchResults = await searchSongs(accessToken, query, limit, offset);
+
+      const tracks = searchResults.tracks.items;
       if (tracks.length > 0) {
         const trackDetailsList = tracks.map((track, index) => {
           const previewUrl = track.preview_url || 'No preview available';
           const image = track.album.images.length > 0 ? track.album.images[0].url : 'No image available';
+
           return {
             id: index + 1,
             trackName: track.name,
@@ -147,16 +180,23 @@ module.exports = async (req, res) => {
             image
           };
         });
-        return res.status(200).json({ tracks: trackDetailsList, developerCredit: 'https://t.me/Teleservices_Api' });
+
+        return res.status(200).json({
+          tracks: trackDetailsList,
+          developerCredit: 'https://t.me/Teleservices_Api'
+        });
       } else {
         return res.status(404).json({ error: 'No tracks found' });
       }
     } else if (bot) {
+      const accessToken = await getAccessToken();
       const trackDetails = await searchSongs(accessToken, bot, 1, 0);
+      
       const trackInfo = trackDetails.tracks.items[0];
       if (trackInfo) {
         const duration_ms = trackInfo.duration_ms;
         const d = Math.floor(duration_ms / 1000);
+
         return res.status(200).json({
           track: {
             trackName: trackInfo.name,
